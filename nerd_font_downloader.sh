535ae89b6bd509a407e130d8c678b3e786adbd5b
aseeralfaisal <aseeralfaisal@gmail.com>
2024-08-24 03:40:39 +0600

    cleanup

--- nerd_font_downloader.sh
+++ nerd_font_downloader.sh
@@ -1,23 +1,36 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-NC='\033[0m' 
+NC='\033[0m'
 
-echo "󰛖 Available from nerdfonts.com: \n"
+echo "󰛖 Available from nerdfonts.com:"
+
 entries=$(curl -s https://www.nerdfonts.com/font-downloads | \
 grep -o 'https://github.com/ryanoasis/nerd-fonts/releases/download/[^"]*' | \
 uniq | sort | \
 awk -F '/releases/download/|/' '{title=$NF; sub(/\.zip$/, "", title); print NR " " title, "link: " $0}')
+
 echo "$entries"
 
 echo "\n󰛖 ${YELLOW}Type ID to install fonts:${NC}"
 read user_id
 
-output=$(echo "$entries" | awk -v id="$user_id" -F ' ' '$1 == id {print $4}')
-wget -P ~/.fonts "$output"
-file=$(basename "$output")
-unzip -d ~/.fonts ~/.fonts/"$file"
-rm -rf ~/.fonts/"$file"
+download_link=$(echo "$entries" | awk -v id="$user_id" -F ' ' '$1 == id {print $4}')
+
+if [ -z "$download_link" ]; then
+  echo "${RED}Invalid ID. Please try again.${NC}"
+  exit 1
+fi
+
+echo "Downloading font..."
+wget -P ~/.fonts "$download_link"
+
+font_file=$(basename "$download_link")
+echo "Unzipping font..."
+unzip -d ~/.fonts ~/.fonts/"$font_file"
+
+rm -rf ~/.fonts/"$font_file"
+
 fc-cache -r
 
-echo "${YELLOW} 󰛖 Installed$filename font "
+echo "${YELLOW}󰛖 Font installed successfully!${NC}"
